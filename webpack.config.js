121f3c004cc357c0d94a2c9e151f1cff20f5cee0
Andrew Varnerin <andrew@varnerin.info>
2016-11-05 14:17:55 -0400

    really no jquery

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: {
     app: "./src/index",
-    vendors: ['angular', 'jquery', 'immutable']
+    vendors: ['angular', 'immutable']
   },
   output: {
     path: "./dist",
